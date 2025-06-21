1983d23b1686c310a461148184adbe65bb6a157b
thomasswf <61506017+thomasswf@users.noreply.github.com>
2025-06-21 16:43:58 +0900

    Update main.jsx
    no google login

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+const dummyUser = {
+  name: 'Local Tester',
+  email: 'test@local.dev',
+  avatar: '/default-avatar.png', // optional if you use avatars
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App user={dummyUser} isLoggedIn={true} />
   </StrictMode>,
-)
+);
